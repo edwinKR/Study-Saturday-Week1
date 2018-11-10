09a126ea8254d83546829664b9058b3d838f2fce
Edwin Kim <edwin.k.kim@gmail.com>
2018-11-10 14:15:09 -0600

    Completed: router.put() for student model. Check how the mutation of arrays and objects work here

---
+++ route/student.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const router = express.Router();
+
+module.exports = router;
+
+let students = [
+  { id: 1, name: 'Dan' },
+  { id: 2, name: 'Karen' },
+  { id: 3, name: 'Hannah' },
+  { id: 4, name: 'Angela' },
+  { id: 5, name: 'Edwin' }
+];
+
+router.get('/', (req, res, next) => {
+  try {
+    res.json(students);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get('/:id', async (req, res, next) => {
+  try {
+    const id = parseInt(req.params.id, 10);
+    const data = students.find(student => {
+      return student.id === id;
+    });
+    res.json(data);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post('/', (req, res, next) => {
+  try {
+    students = [...students, req.body];
+    console.log('Posting Status is~~~~ ', students);
+    res.json(req.body);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.put('/:id', (req, res, next) => {
+  try {
+    let parsedId = parseInt(req.params.id, 10);
+    students = students.map(student => {
+      if (student.id === parsedId) {
+        console.log('Before mutating: ', student);
+        student = { ...student, ...req.body };
+        console.log('After mutating: ', student);
+      }
+      return student;
+    });
+    console.log('Final student database after put(update): ', students);
+    res.json(req.body);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.delete('/:id', (req, res, next) => {});
